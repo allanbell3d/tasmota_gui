6344af4dba094fb0055c1d28b224978def3061bc
allanbell3d <36531528+allanbell3d@users.noreply.github.com>
2026-01-06 11:13:56 +0400

    Update build_release_json_embeded.ps1
    faulty commands.py file path

--- build_release_json_embeded.ps1
+++ build_release_json_embeded.ps1
@@ -34,7 +34,7 @@ try {
 
 Write-Host "🔍 Using project root: $ProjectRoot"
 
-$ConstantsFile   = Join-Path $ProjectRoot "constants.py"
+$ConstantsFile   = Join-Path $ProjectRoot "src\tastmota\constants.py"
 $EntryScript     = Join-Path $ProjectRoot "apps\desktop.py"
 $SourcePath      = Join-Path $ProjectRoot "src"
 $CommandLibrary  = Join-Path $ProjectRoot "assets\commands\tasmota_commands.json"
